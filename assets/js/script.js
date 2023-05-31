87338c61ee316810a64c259a4d6929439aadf15e
Joe📸 <joe.@Joes-MBP.lan>
2023-05-31 11:38:38 -0500

    jones-updating: html: add place holders. JS: check for all input fiels. need to clear input fields after push, dropdown or transfrorm into a card wit a button push showing information

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,34 @@
-document.querySelector('#push').onclick = function(){
-    if(document.querySelector('#newtask input').value.length == 0){
-        alert("Kindly Enter Task Name!!!!")
+document.querySelector('#push').onclick = function() {
+    var inputs = document.querySelectorAll('#newtask input');
+    var isEmpty = false;
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value.trim() === '') {
+            isEmpty = true;
+            break;
+        }
     }
 
-    else{
+    if (isEmpty) {
+        alert("Kindly fill in all input fields!");
+    } else {
+        
         document.querySelector('#tasks').innerHTML += `
-            <div class="task">
-                <span id="taskname">
-                    ${document.querySelector('#newtask input').value}
-                </span>
-                <button class="delete">
-                    <i class="far fa-trash-alt"></i>
-                </button>
-            </div>
-        `;
+        <div class="task">
+            <span id="taskname">
+                ${document.querySelector('#newtask input').value}
+            </span>
+            <button class="delete">
+                <i class="far fa-trash-alt"></i>
+            </button>
+        </div>
+    `;
 
-        var current_tasks = document.querySelectorAll(".delete");
-        for(var i=0; i<current_tasks.length; i++){
-            current_tasks[i].onclick = function(){
-                this.parentNode.remove();
-            }
+    var current_tasks = document.querySelectorAll(".delete");
+    for(var i=0; i<current_tasks.length; i++){
+        current_tasks[i].onclick = function(){
+            this.parentNode.remove();
         }
+    }
     }
 }
