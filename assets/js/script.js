89077b67e01f15baeb99b48392419458b164386b
Joe📸 <joe.@Joes-MBP.lan>
2023-06-01 08:15:46 -0500

    edit file structure, functionality through task 4 is good. CSS needs work.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,107 +1,73 @@
-// document.querySelector('#push').onclick = function() {
-//     var inputs = document.querySelectorAll('#newtask input');
-//     var isEmpty = false;
 
-//     for (var i = 0; i < inputs.length; i++) {
-//         if (inputs[i].value.trim() === '') {
-//             isEmpty = true;
-//             break;
-//         }
-//     }
+document.getElementById('taskForm').addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevent form submission
 
-//     if (isEmpty) {
-//         alert("Kindly fill in all input fields!");
-//     } else {
-        
-//         document.querySelector('#tasks').innerHTML += `
-//         <div class="task">
-//             <span id="taskname">
-//                 ${document.querySelector('#newtask input').value}
-//             </span>
-//             <button class="delete">
-//                 <i class="far fa-trash-alt"></i>
-//             </button>
-//         </div>
-//     `;
+    // Retrieve form field values
+    const taskName = document.getElementById('taskName').value;
+    const taskDescription = document.getElementById('taskDescription').value;
+    const taskAssignedTo = document.getElementById('taskAssignedTo').value;
+    const taskDueDate = document.getElementById('taskDueDate').value;
+    const taskStatus = document.getElementById('taskStatus').value;
 
-//     var current_tasks = document.querySelectorAll(".delete");
-//     for(var i=0; i<current_tasks.length; i++){
-//         current_tasks[i].onclick = function(){
-//             this.parentNode.remove();
-//         }
-//     }
-    
-//     //clears input field
-//     for(var i=0; i < inputs.length; i++){
-//         inputs[i].value = "";
-//     }
+    // Validate form fields
+    if (taskName.trim() === '') {
+        showError('Please enter a task name');
+        return;
+    }
 
-//     }
-// }
+    if (taskDescription.trim() === '') {
+        showError('Please enter a task description');
+        return;
+    }
 
-
-// test code
-
-function createCardFactory() {
-    var cardFactory = {};
-  
-    cardFactory.createCard = function() {
-      var inputs = document.querySelectorAll('#newtask input');
-      var isEmpty = false;
-  
-      for (var i = 0; i < inputs.length; i++) {
-        if (inputs[i].value.trim() === '') {
-          isEmpty = true;
-          break;
-        }
-      }
-  
-      if (isEmpty) {
-        alert("Kindly fill in all input fields!");
-      } else {
-        var cardName = document.querySelector('#newtask input[name="taskname"]').value;
-        var cardDescription = document.querySelector('#newtask input[name="description"]').value;
-        var cardAssignedTo = document.querySelector('#newtask input[name="assignedto"]').value;
-        var cardDueDate = document.querySelector('#newtask input[name="duedate"]').value;
-        var cardStatus = document.querySelector('#newtask input[name="status"]').value;
-  
-        var cardElement = document.createElement('div');
-        cardElement.classList.add('task');
-  
-        var cardContent = document.createElement('span');
-        cardContent.id = 'taskname';
-        cardContent.textContent = cardName;
-  
-        var deleteButton = document.createElement('button');
-deleteButton.classList.add('delete');
-deleteButton.innerHTML = '<i class="far fa-trash-alt"></i>';
-
-deleteButton.onclick = function() {
-  var confirmDelete = confirm("Are you sure you want to delete this task?");
-  if (confirmDelete) {
-    cardElement.remove();
-  }
-};
-
-  
-        cardElement.appendChild(cardContent);
-        cardElement.appendChild(deleteButton);
-  
-        document.querySelector('#tasks').appendChild(cardElement);
-  
-        // Clear input fields
-        for (var i = 0; i < inputs.length; i++) {
-          inputs[i].value = '';
-        }
-      }
+    // Create task object
+    const task = {
+        name: taskName,
+        description: taskDescription,
+        assignedTo: taskAssignedTo,
+        dueDate: taskDueDate,
+        status: taskStatus
     };
-  
-    return cardFactory;
-  }
-  
-  var cardFactory = createCardFactory();
-  
-  document.querySelector('#push').onclick = function() {
-    cardFactory.createCard();
-  };
-  
+
+    // Call function to add task to the list
+    addTaskToList(task);
+
+    // Clear form fields
+    document.getElementById('taskForm').reset();
+});
+
+// Function to add task to the list
+function addTaskToList(task) {
+    const taskList = document.getElementById('taskList');
+
+    // Create list item for task
+    const taskItem = document.createElement('li');
+    taskItem.classList.add('list-group-item');
+    taskItem.innerHTML = `
+        <div class="d-flex justify-content-between align-items-center">
+            <h5>${task.name}</h5>
+            <span class="badge badge-info">${task.status}</span>
+        </div>
+        <p>${task.description}</p>
+        <p><strong>Assigned to:</strong> ${task.assignedTo}</p>
+        <p><strong>Due Date:</strong> ${task.dueDate}</p>
+    `;
+
+    // Append task item to task list
+    taskList.appendChild(taskItem);
+}
+
+// Function to show error message
+function showError(message) {
+    const errorDiv = document.createElement('div');
+    errorDiv.classList.add('alert', 'alert-danger');
+    errorDiv.textContent = message;
+
+    const form = document.getElementById('taskForm');
+    form.insertBefore(errorDiv, form.firstChild);
+
+    // Clear error after 3 seconds
+    setTimeout(function () {
+        errorDiv.remove();
+    }, 3000);
+}
