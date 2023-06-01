4ba50adbf910baf100548557798fcb55a252ffea
Joe📸 <joe.@Joes-MBP.lan>
2023-06-01 13:36:34 -0500

    functions up to task 6. Importing and exporting modules seems off. card is formated

--- assets/js/TaskManager.js
+++ assets/js/TaskManager.js
@@ -1,10 +1,9 @@
 
 
-
-// assets/js/TaskManager.js
-
 // Create a class to manage tasks
-class TaskManager {
+
+
+ class TaskManager {
     constructor() {
         this.tasks = [];
         this.currentId = 0;
@@ -35,11 +34,11 @@ class TaskManager {
 
         this.tasks.forEach((task) => {
             const taskElement = document.createElement("li");
-            taskElement.classList.add("task-item");
+            taskElement.classList.add("list-group-item");
             taskElement.innerHTML = `
-                <div class="task-header">
+                <div class="task-header d-flex justify-content-between align-items-center">
                     <span>${task.name}</span>
-                    <button class="delete-button" data-task-id="${task.id}">Delete</button>
+                    <button class="delete-button btn btn-danger" data-task-id="${task.id}">Delete</button>
                 </div>
                 <div class="task-details">
                     <span class="badge ${task.status}">${task.status}</span>
@@ -60,6 +59,42 @@ class TaskManager {
     }
 }
 
-// Export the TaskManager class
-export default {TaskManager};
+// Create an instance of TaskManager
+const taskManager = new TaskManager();
+
+// Add event listener to the task form
+const taskForm = document.getElementById("taskForm");
+taskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // Get the form inputs
+    const nameInput = document.getElementById("name");
+    const descriptionInput = document.getElementById("description");
+    const assignedToInput = document.getElementById("assignedTo");
+    const dueDateInput = document.getElementById("dueDate");
+    const statusInput = document.getElementById("status");
+
+    // Get the input values
+    const name = nameInput.value;
+    const description = descriptionInput.value;
+    const assignedTo = assignedToInput.value;
+    const dueDate = dueDateInput.value;
+    const status = statusInput.value;
+
+    // Add the task
+    taskManager.addTask(name, description, assignedTo, dueDate, status);
+
+    // Clear the form inputs
+    nameInput.value = "";
+    descriptionInput.value = "";
+    assignedToInput.value = "";
+    dueDateInput.value = "";
+    statusInput.value = "TODO";
+
+    // Render the tasks
+    taskManager.renderTasks();
+});
+
+// Render the initial tasks
+taskManager.renderTasks();
 
